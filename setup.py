50992b44de5c721fca446841f72039a96bb42c36
Zabana <karim.cheurfi@gmail.com>
2019-08-13 20:03:14 +0200

    created setup.py file for PyPI.org

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from chevette.constants import VERSION
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='chevette',
+    version=VERSION,
+    description='Chevette is a MarkDown powered static blog engine.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(
+        exclude=['docs', 'tests']
+    ),
+    url="https://github.com/Zabanaa/chevette",
+    author="Karim C",
+    email="karim.cheurfi@gmail.com",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: WTFPL License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary'
+    ],
+    keywords="static site generator",
+    python_requires='>=3.7',
+    install_requires=[
+        'jinja2',
+        'misaka',
+        'python-frontmatter',
+        'click',
+        'colorama'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chevette=chevette.cli:chevette'
+        ]
+    }
+)
