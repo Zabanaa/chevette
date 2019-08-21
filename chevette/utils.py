9053096a6c0ad1e7f2adfb2cc0d5a9994287688f
Zabana <karim.cheurfi@gmail.com>
2019-08-21 16:12:16 +0200

    different changes

--- chevette/utils.py
+++ chevette/utils.py
@@ -1,10 +1,12 @@
+import sys
 import os
 import codecs
 import frontmatter as fm
 import misaka as m
 from shutil import rmtree
+from colorama import Fore, Style
 from chevette.constants import (
-    JINJA_ENV,
+    MAIN_JINJA_ENV,
     OUTPUT_DIR,
     EXTENSIONS_NOT_ALLOWED
 )
@@ -15,12 +17,21 @@ def _is_markdown(file):
 
 
 def _is_extention_allowed(file):
-    file_ext = file.split('.')[1]
+    try:
+        file_ext = file.split('.')[1]
+    except IndexError:
+        return False
     return file_ext not in EXTENSIONS_NOT_ALLOWED
 
 
 def folder_exists(path):
     return os.path.isdir(path)
+
+
+def _print_error_and_exit(message):
+    print(Fore.RED + message.strip())
+    print(Style.RESET_ALL)
+    sys.exit(1)
 
 
 def clear_directory(_dir):
@@ -29,7 +40,7 @@ def clear_directory(_dir):
 
         if _is_file(file_path):
             os.unlink(file_path)
-        elif _is_dir(file_path):
+        elif folder_exists(file_path):
             rmtree(file_path)
 
 
@@ -37,16 +48,12 @@ def _is_file(path):
     return os.path.isfile(path)
 
 
-def _is_dir(path):
-    return os.path.isdir(path)
-
-
 def _get_html_filename(path):
     filename = os.path.basename(path)
     return os.path.splitext(filename)[0] + '.html'
 
 
-def _render_file_to_html(metadata, content, html_filename):
+def _render_file_to_html(metadata, content, html_filename, file):
 
     cur_dir = os.getcwd()
     public_path = os.path.join(
@@ -57,7 +64,15 @@ def _render_file_to_html(metadata, content, html_filename):
     content = m.html(content)
 
     with codecs.open(public_path, 'w', 'utf-8') as fd:
-        layout = metadata.get('layout')
+        try:
+            layout = metadata['layout']
+        except KeyError:
+            err_msg = f"""
+                [Error]: in file {file}.
+                Missing 'layout' key in front matter
+            """
+            _print_error_and_exit(err_msg)
+
         template = JINJA_ENV.get_template(f'layouts/{layout}.html.jinja2')
         fd.write(template.render(
             content=content,
@@ -69,7 +84,7 @@ def _render_file_to_html(metadata, content, html_filename):
 def _render_markdown_page(file):
     html_filename = _get_html_filename(file)
     metadata, content = _parse_markdown_file(file)
-    _render_file_to_html(metadata, content, html_filename)
+    _render_file_to_html(metadata, content, html_filename, file)
 
 
 def _parse_markdown_file(file):
@@ -82,6 +97,6 @@ def render_template_to_file(path, new_file, _vars={}):
     # one that renders he template
     # the other that saves the file
     with codecs.open(os.path.join(path, new_file), 'w', 'utf-8') as fd:
-        template = JINJA_ENV.get_template(f'{new_file}.jinja2')
+        template = MAIN_JINJA_ENV.get_template(f'{new_file}.jinja2')
         fd.write(template.render(**_vars))
         fd.close()
