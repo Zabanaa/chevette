8c200018bc56ae0ab467b4af9bcb4ff0df92da9c
Zabana <karim.cheurfi@gmail.com>
2019-08-28 17:22:17 +0200

    remove SITE_ prefix from settings.py template and update base template to reflect the config generation logic changes

--- chevette/templates/settings.py
+++ chevette/templates/settings.py
@@ -1,12 +1,12 @@
-SITE_TITLE = 'Chevette - Static Blog Engine'
-SITE_AUTHOR = 'Mr. K'
+TITLE = 'Chevette - Static Blog Engine'
+AUTHOR = 'Mr. K'
 EMAIL = 'your.email.address@example.com'
-SITE_DESCRIPTION = """
+META_DESCRIPTION = """
     Your website's meta description. This is particularly
     useful for search engines as this will appear inside the 
     website's <head> tag.
 """
-BASE_URL = ''
-SITE_URL = ''
+BASE_URL = '/'
+SITE_URL = 'mysite.xyz'
 TWITTER_USERNAME = '@zabanaa_'
 GITHUB_USERNAME = 'zabanaa'
